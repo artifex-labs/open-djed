b7d4f1410011920b717efc64b6946d5ed3783f6d
Kylix Afonso <kylixafonso@gmail.com>
2025-04-13 23:35:25 +0100

    feat(math): add min and max reserve ratios

--- packages/math/src/reserve.ts
+++ packages/math/src/reserve.ts
@@ -7,3 +7,9 @@ export const adaInReserve = ({ adaInReserve }: Pick<PoolDatum, 'adaInReserve'>):
 
 export const reserveRatio = (poolDatum: PartialPoolDatum, oracleDatum: PartialOracleDatum): Rational =>
   new Rational(adaInReserve(poolDatum)).div(djedADARate(oracleDatum).mul(poolDatum.djedInCirculation))
+
+// Ratio of ADA in reserve to DJED in circulation over which SHEN is no longer mintable. Not sure if this is configurable.
+export const maxReserveRatio = 8
+
+// Ratio of ADA in reserve to DJED in circulation under which DJED is no longer mintable. Not sure if this is configurable.
+export const minReserveRatio = 4
