f2a79d18cb7da4cd09de2e5f06191507f9473212
Kylix Afonso <kylixafonso@gmail.com>
2025-04-20 10:29:07 +0100

    feat(api): implement schema

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,18 +6,52 @@ import { env } from './env'
 
 const app = new Hono()
 
-const registry = registryByNetwork[env.VITE_NETWORK]
+app.get('/api/:token/:action/:quantity/data', (c) => {
+  return c.json({
+    base_cost: 0,
+    operator_fee: 0,
+    cost: 0,
+    min_ada: 0,
+  })
+})
 
-app.get('/', (c) => c.text(`Hello Cardano ${env.VITE_NETWORK} network!`))
+app.get('/api/:token/:action/:quantity/tx', (c) => {
+  // deconstruct body, which should be a json object with the following fields:
+  // - address: string
+  // - utxos: string[]
+  return c.text('<cbor-here>')
+})
 
-app.get('/pool-utxo', async (c) => {
-  const lucid = await Lucid(
-    new Blockfrost(env.VITE_BLOCKFROST_URL, env.VITE_BLOCKFROST_PROJECT_ID),
-    env.VITE_NETWORK,
-  )
+app.get('/api/protocol_data', (c) => {
+  return c.json({
+    djed: {
+      buy_price: 0,
+      sell_price: 0,
+      circulating_supply: 0,
+      mintable_amount: 0,
+    },
+    shen: {
+      buy_price: 0,
+      sell_price: 0,
+      circulating_supply: 0,
+      mintable_amount: 0,
+    },
+    reserve: {
+      amount: 0,
+      ratio: 0,
+    },
+  })
+})
 
-  const poolUTxO = await lucid.utxoByUnit(registry.poolAssetId)
-  return c.text(`Pool UTxO has ref, ${poolUTxO.txHash}#${poolUTxO.outputIndex}.`)
+app.get('/api/orders', (c) => {
+  return c.json([])
+})
+
+app.get('/api/cancel-order-tx/:order_out_ref', (c) => {
+  // deconstruct body, which should be a json object with the following fields:
+  // - address: string
+  // - utxos: string[]
+  return c.text('<cbor-here>')
 })
 
 export default app
