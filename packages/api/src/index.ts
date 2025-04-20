aca65e7b57212921de35e876e503d4e1dbfef7ca
Kylix Afonso <kylixafonso@gmail.com>
2025-04-20 18:25:41 +0100

    feat(api): implement `protocol-data` endpoint

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,44 +1,107 @@
 import * as _CML from '@anastasia-labs/cardano-multiplatform-lib-browser'
-import { Blockfrost, Lucid } from '@lucid-evolution/lucid'
+import { Blockfrost, Lucid, Data } from '@lucid-evolution/lucid'
 import { registryByNetwork } from '@reverse-djed/txs'
 import { Hono } from 'hono'
 import { env } from './env'
+import { z } from 'zod'
+import { MyBlockfrost } from '../../cli/src/blockfrost'
+import { OracleDatum, PoolDatum } from '@reverse-djed/data'
+import {
+  djedADABurnRate,
+  djedADAMintRate,
+  maxMintableDJED,
+  maxMintableSHEN,
+  reserveRatio,
+  shenADABurnRate,
+  shenADAMintRate,
+} from '@reverse-djed/math'
 
 const app = new Hono()
 
+const tokenSchema = z.enum(['DJED', 'SHEN'])
+const actionSchema = z.enum(['mint', 'burn'])
+
 app.get('/api/:token/:action/:quantity/data', (c) => {
+  const token = tokenSchema.parse(c.req.param('token'))
+  const action = actionSchema.parse(c.req.param('action'))
+  const quantity = BigInt(Math.floor(Number(c.req.param('quantity')) * 10e6))
+  if (quantity < 0n) {
+    throw new Error('Quantity must be positive number.')
+  }
   return c.json({
     base_cost: 0,
-    operator_fee: 0,
+    operator_fee: 0n,
     cost: 0,
     min_ada: 0,
   })
 })
 
-app.get('/api/:token/:action/:quantity/tx', (c) => {
-  // deconstruct body, which should be a json object with the following fields:
-  // - address: string
-  // - utxos: string[]
+const txRequestBodySchema = z.object({
+  address: z.string(),
+  utxos: z.array(z.string()),
+})
+
+app.post('/api/:token/:action/:quantity/tx', async (c) => {
+  const token = tokenSchema.parse(c.req.param('token'))
+  const action = actionSchema.parse(c.req.param('action'))
+  const quantity = BigInt(Math.floor(Number(c.req.param('quantity')) * 10e6))
+  if (quantity < 0n) {
+    throw new Error('Quantity must be positive number.')
+  }
+  const { address, utxos } = txRequestBodySchema.parse(await c.req.json())
   return c.text('<cbor-here>')
 })
 
-app.get('/api/protocol_data', (c) => {
+app.get('/api/protocol_data', async (c) => {
+  const lucid = await Lucid(
+    new MyBlockfrost(
+      `https://cardano-${env.VITE_NETWORK.toLocaleLowerCase()}.blockfrost.io/api/v0`,
+      env.VITE_BLOCKFROST_PROJECT_ID,
+    ),
+    env.VITE_NETWORK,
+  )
+  const registry = registryByNetwork[env.VITE_NETWORK]
+
+  const rawPoolUTxO = (await lucid.utxosAtWithUnit(registry.poolAddress, registry.poolAssetId))[0]
+  if (!rawPoolUTxO) throw new Error(`Couldn't find pool utxo.`)
+  const poolUTxO = {
+    ...rawPoolUTxO,
+    poolDatum: Data.from(Data.to(await lucid.datumOf(rawPoolUTxO)), PoolDatum),
+  }
+  const rawOracleUTxO = (await lucid.utxosAtWithUnit(registry.oracleAddress, registry.oracleAssetId))[0]
+  if (!rawOracleUTxO) throw new Error(`Couldn't find oracle utxo.`)
+  const oracleUTxO = {
+    ...rawOracleUTxO,
+    oracleDatum: Data.from(Data.to(await lucid.datumOf(rawOracleUTxO)), OracleDatum),
+  }
   return c.json({
     djed: {
-      buy_price: 0,
-      sell_price: 0,
-      circulating_supply: 0,
-      mintable_amount: 0,
+      buy_price: djedADAMintRate(oracleUTxO.oracleDatum, registry.mintDJEDFeePercentage).toNumber(),
+      sell_price: djedADABurnRate(oracleUTxO.oracleDatum, registry.burnDJEDFeePercentage).toNumber(),
+      circulating_supply: Number(poolUTxO.poolDatum.djedInCirculation) / 1e6,
+      mintable_amount:
+        Number(maxMintableDJED(poolUTxO.poolDatum, oracleUTxO.oracleDatum, registry.mintDJEDFeePercentage)) /
+        1e6,
     },
     shen: {
-      buy_price: 0,
-      sell_price: 0,
-      circulating_supply: 0,
-      mintable_amount: 0,
+      buy_price: shenADAMintRate(
+        poolUTxO.poolDatum,
+        oracleUTxO.oracleDatum,
+        registry.mintSHENFeePercentage,
+      ).toNumber(),
+      sell_price: shenADABurnRate(
+        poolUTxO.poolDatum,
+        oracleUTxO.oracleDatum,
+        registry.burnSHENFeePercentage,
+      ).toNumber(),
+      circulating_supply: Number(poolUTxO.poolDatum.shenInCirculation) / 1e6,
+      mintable_amount:
+        Number(maxMintableSHEN(poolUTxO.poolDatum, oracleUTxO.oracleDatum, registry.mintSHENFeePercentage)) /
+        1e6,
     },
     reserve: {
-      amount: 0,
-      ratio: 0,
+      amount: Number(poolUTxO.poolDatum.adaInReserve) / 1e6,
+      ratio: reserveRatio(poolUTxO.poolDatum, oracleUTxO.oracleDatum).toNumber(),
     },
   })
 })
@@ -47,10 +110,9 @@ app.get('/api/orders', (c) => {
   return c.json([])
 })
 
-app.get('/api/cancel-order-tx/:order_out_ref', (c) => {
-  // deconstruct body, which should be a json object with the following fields:
-  // - address: string
-  // - utxos: string[]
+app.post('/api/cancel-order-tx/:order_out_ref', async (c) => {
+  const orderOutRef = tokenSchema.parse(c.req.param('order_out_ref'))
+  const { address, utxos } = txRequestBodySchema.parse(await c.req.json())
   return c.text('<cbor-here>')
 })
 
