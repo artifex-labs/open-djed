c7e3137b7377d0461d24b95133ac9d2f3cd9d87a
Kylix Afonso <kylixafonso@gmail.com>
2025-04-20 21:24:37 +0100

    feat(api): fill in `/api/cancel-order-ts` endpoint

--- packages/txs/src/utils.ts
+++ packages/txs/src/utils.ts
@@ -4,11 +4,11 @@ export const parseOutRef = (outRefStr: string): OutRef => {
   const txOutRef = outRefStr.match(
     // In words, a txOutRef starts with 64 hex characters, followed by '#',
     // followed by up to 2 digits, then ends.
-    /^(?<txHash>[a-f0-9]{64})#(?<index>\d{1,2})$/,
+    /^(?<txHash>[0-9a-fA-F]{64})(?<separator>[#\-\.])(?<index>\d+)$/,
   )
 
   if (!txOutRef?.groups?.txHash || !txOutRef?.groups?.index) {
-    throw new Error(`Expected a txOutRef in <tx-hash>#<index> format, got: "${outRefStr}"`)
+    throw new Error(`Expected a txOutRef in <tx-hash><separator><index> format, got: "${outRefStr}"`)
   }
 
   return { txHash: txOutRef.groups.txHash, outputIndex: Number(txOutRef.groups.index) }
