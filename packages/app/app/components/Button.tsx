9c305de6f7b0ec8e8474e316e94ba77929227202
Gil <rafaelgil131@gmail.com>
2025-05-02 00:26:44 +0100

    feat(app): Implemented header UI

--- packages/app/app/components/Button.tsx
+++ packages/app/app/components/Button.tsx
@@ -12,7 +12,7 @@ type ButtonProps = {
 const Button: React.FC<ButtonProps> = ({ children, onClick, className, ref, disabled }) => {
   return (
     <button
-      className={`border-1 border-black rounded-md p-2 font-bold ${disabled ? 'bg-gray-500 text-secondary cursor-not-allowed' : ''}  hover:bg-gray-100 transition-opacity px-4 py-2 rounded-lg cursor-pointer ${className} `}
+      className={`rounded-md p-2 text-white font-bold ${disabled ? 'bg-light-disabled dark:bg-dark-disabled opacity-50 cursor-not-allowed' : 'bg-primary'}  hover:bg-primary-hover transition-opacity px-4 py-2 rounded-lg cursor-pointer ${className} `}
       onClick={onClick}
       disabled={disabled}
       ref={ref}
--- packages/app/app/components/ThemeToggle.tsx
+++ packages/app/app/components/ThemeToggle.tsx
@@ -1,12 +1,13 @@
 import { useTheme } from '../context/ThemeContext'
 import { FiSun, FiMoon } from 'react-icons/fi'
+import Button from './Button'
 
 export const ThemeToggle = () => {
   const { isDarkMode, toggleTheme } = useTheme()
 
   return (
-    <button onClick={toggleTheme} className="p-2 rounded-lg bg-gray-200 dark:bg-gray-700 transition-colors">
+    <Button onClick={toggleTheme} className="text-white transition-colors">
       {isDarkMode ? <FiSun className="w-5 h-5" /> : <FiMoon className="w-5 h-5" />}
-    </button>
+    </Button>
   )
 }
