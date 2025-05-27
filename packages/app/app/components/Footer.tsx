eb5c0a5538ec080eb20066b2dd845fe522a44610
Salgueirinho <goncalosalgueirinho842@gmail.com>
2025-05-27 18:52:28 +0100

    feat(app): add daisyUI tooltips

--- packages/app/app/components/Footer.tsx
+++ packages/app/app/components/Footer.tsx
@@ -7,73 +7,110 @@ const Footer = () => {
       </div>
 
       <div className="flex flex-col md:flex-row gap-6 items-center">
-        <a
-          href="https://discord.gg/MhYP7w8n8p"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-primary focus:outline-none transition-colors"
-          title="Join community discord server"
-        >
-          <img src="/discord-logo.png" alt="Discord Logo" className="w-[20px] inline-block mr-2" />
-          Discord
-        </a>
-        <a
-          href="https://x.com/ArtifexLab50024"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-primary focus:outline-none transition-colors"
-          title="Follow us for more news"
-        >
-          <img src="/twitter-logo.png" alt="Twitter Logo" className="w-[20px] inline-block mr-2" />
-          Twitter
-        </a>
-        <a
-          href="https://github.com/artifex-labs/reverse-djed"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-primary focus:outline-none transition-colors"
-          title="Look at reverse djed source code"
-        >
-          <img src="/github-logo.png" alt="GitHub Logo" className="w-[20px] inline-block mr-2" />
-          GitHub
-        </a>
-        <a
-          href="https://djed.xyz"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-primary focus:outline-none transition-colors"
-          title="Official djed app"
-        >
-          <img src="/djed.svg" alt="GitHub Logo" className="w-[20px] inline-block mr-2" />
-          djed.xyz
-        </a>
-        <a
-          href="https://status.artifex.finance/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-primary focus:outline-none transition-colors flex items-center gap-1"
-          title="Service status page"
-        >
-          <i className="fas fa-heartbeat text-red-500"></i>
-          <span>Status</span>
-        </a>
-        <a
-          href="/terms"
-          className="hover:text-primary focus:outline-none transition-colors flex items-center gap-1"
-          title="Terms of Service"
-        >
-          <i className="fas fa-file-contract text-primary-500"></i>
-          <span>Terms</span>
-        </a>
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">
+              Join our community Discord server
+            </div>
+          </div>
+          <a
+            href="https://discord.gg/MhYP7w8n8p"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-primary focus:outline-none transition-colors"
+          >
+            <img src="/discord-logo.png" alt="Discord Logo" className="w-[20px] inline-block mr-2" />
+            Discord
+          </a>
+        </div>
 
-        <a
-          href="/privacy"
-          className="hover:text-primary focus:outline-none transition-colors flex items-center gap-1"
-          title="Privacy Policy"
-        >
-          <i className="fas fa-user-secret text-primary-500"></i>
-          <span>Privacy</span>
-        </a>
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">Follow us for more news</div>
+          </div>
+          <a
+            href="https://x.com/artifex_labs"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-primary focus:outline-none transition-colors"
+          >
+            <img src="/twitter-logo.png" alt="Twitter Logo" className="w-[20px] inline-block mr-2" />
+            Twitter
+          </a>
+        </div>
+
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">
+              Look at reverse djed source code
+            </div>
+          </div>
+          <a
+            href="https://github.com/artifex-labs/reverse-djed"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-primary focus:outline-none transition-colors"
+          >
+            <img src="/github-logo.png" alt="GitHub Logo" className="w-[20px] inline-block mr-2" />
+            GitHub
+          </a>
+        </div>
+
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">Official djed app</div>
+          </div>
+          <a
+            href="https://djed.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-primary focus:outline-none transition-colors"
+          >
+            <img src="/djed.svg" alt="GitHub Logo" className="w-[20px] inline-block mr-2" />
+            djed.xyz
+          </a>
+        </div>
+
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">Service status page</div>
+          </div>
+          <a
+            href="https://status.artifex.finance/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-primary focus:outline-none transition-colors flex items-center gap-1"
+          >
+            <i className="fas fa-heartbeat text-red-500"></i>
+            <span>Status</span>
+          </a>
+        </div>
+
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">Terms of Service</div>
+          </div>
+          <a
+            href="/terms"
+            className="hover:text-primary focus:outline-none transition-colors flex items-center gap-1"
+          >
+            <i className="fas fa-file-contract text-primary-500"></i>
+            <span>Terms</span>
+          </a>
+        </div>
+
+        <div className="tooltip">
+          <div className="tooltip-content">
+            <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">Privacy Policy</div>
+          </div>
+          <a
+            href="/privacy"
+            className="hover:text-primary focus:outline-none transition-colors flex items-center gap-1"
+          >
+            <i className="fas fa-user-secret text-primary-500"></i>
+            <span>Privacy</span>
+          </a>
+        </div>
       </div>
     </footer>
   )
