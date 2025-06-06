b430c570e2b53dffe5da16cace49a1675e22d08a
Kylix Afonso <kylixafonso@gmail.com>
2025-06-06 23:31:17 +0100

    refactor(app): use `Value` type more

--- packages/app/app/utils.ts
+++ packages/app/app/utils.ts
@@ -1,3 +1,12 @@
+import {
+  adaDJEDRate,
+  adaSHENRate,
+  djedADARate,
+  shenADARate,
+  type PartialOracleDatum,
+  type PartialPoolDatum,
+} from '@reverse-djed/math'
+
 export function formatNumber(
   value: number,
   options: {
@@ -12,3 +21,89 @@ export function formatNumber(
 }
 
 export const DEFAULT_SHOW_BALANCE = true
+
+const VALUE_KEYS = ['ADA', 'DJED', 'SHEN']
+
+export const formatValue = (value: Value) => {
+  const filteredValue = Object.entries(value).filter(([, v]) => v && v > 0)
+  if (filteredValue.length === 0) return `0 ADA`
+  return filteredValue
+    .sort((a, b) => VALUE_KEYS.indexOf(a[0]) - VALUE_KEYS.indexOf(b[0]))
+    .map(([k, v]) => `${formatNumber(v, { maximumFractionDigits: 4 })} ${k}`)
+    .join(' ')
+}
+
+export type ADAValue = {
+  ADA: number
+}
+
+export type TokenValue = {
+  DJED: {
+    DJED: number
+  }
+  SHEN: {
+    SHEN: number
+  }
+}
+
+export type Value = Partial<Record<'ADA' | 'DJED' | 'SHEN', number>>
+
+export const sumValues = (...values: Value[]): Value =>
+  values.reduce(
+    (acc, value) => ({
+      ...acc,
+      ...Object.fromEntries(Object.entries(value).map(([k, v]) => [k, v + (acc[k as keyof Value] ?? 0)])),
+    }),
+    {},
+  )
+
+export const valueToDJED = (
+  value: Value,
+  poolDatum: PartialPoolDatum,
+  oracleDatum: PartialOracleDatum,
+): number =>
+  adaDJEDRate(oracleDatum)
+    .mul(
+      shenADARate(poolDatum, oracleDatum)
+        .mul(BigInt(Math.floor((value.SHEN ?? 0) * 1e6)))
+        .add(BigInt(Math.floor((value.ADA ?? 0) * 1e6))),
+    )
+    .div(1_000_000n)
+    .toNumber() + (value.DJED ?? 0)
+
+export const valueToADA = (
+  value: Value,
+  poolDatum: PartialPoolDatum,
+  oracleDatum: PartialOracleDatum,
+): number =>
+  shenADARate(poolDatum, oracleDatum)
+    .mul(BigInt(Math.floor((value.SHEN ?? 0) * 1e6)))
+    .add(djedADARate(oracleDatum).mul(BigInt(Math.floor((value.DJED ?? 0) * 1e6))))
+    .div(1_000_000n)
+    .toNumber() + (value.ADA ?? 0)
+
+export const valueToSHEN = (
+  value: Value,
+  poolDatum: PartialPoolDatum,
+  oracleDatum: PartialOracleDatum,
+): number =>
+  adaSHENRate(poolDatum, oracleDatum)
+    .mul(
+      djedADARate(oracleDatum)
+        .mul(BigInt(Math.floor((value.DJED ?? 0) * 1e6)))
+        .add(BigInt(Math.floor(value.ADA ?? 0) * 1e6)),
+    )
+    .div(1_000_000n)
+    .toNumber() + (value.SHEN ?? 0)
+
+export const valueTo = (
+  value: Value,
+  poolDatum: PartialPoolDatum,
+  oracleDatum: PartialOracleDatum,
+  token: 'DJED' | 'SHEN' | 'ADA',
+): number =>
+  token === 'DJED'
+    ? valueToDJED(value, poolDatum, oracleDatum)
+    : token === 'SHEN'
+      ? valueToSHEN(value, poolDatum, oracleDatum)
+      : valueToADA(value, poolDatum, oracleDatum)
