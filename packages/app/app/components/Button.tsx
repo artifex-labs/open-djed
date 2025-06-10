0eaf8c448edc3a2c4d0d9bd59d0ba01350740374
pmsnnogueira <pmsn.mig2002@gmail.com>
2025-06-10 16:14:19 +0100

    test(app): test terms

--- packages/app/app/components/Button.tsx
+++ packages/app/app/components/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 type ButtonProps = {
+  name?: string
   children: React.ReactNode
   onClick?: () => void
   className?: string
@@ -10,7 +11,7 @@ type ButtonProps = {
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'full'
 }
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, className, ref, disabled, size = 'md' }) => {
+const Button: React.FC<ButtonProps> = ({ name, children, onClick, className, ref, disabled, size = 'md' }) => {
   const sizeClass = {
     xs: 'w-13',
     sm: 'w-24',
@@ -21,6 +22,7 @@ const Button: React.FC<ButtonProps> = ({ children, onClick, className, ref, disa
 
   return (
     <button
+      name={name}
       className={`${sizeClass} text-white font-bold bg-primary flex items-center justify-center h-auto ${
         disabled ? 'opacity-30 cursor-not-allowed' : 'hover:bg-primary-hover cursor-pointer'
       } transition-opacity px-4 py-2 rounded-lg ${className}`}
