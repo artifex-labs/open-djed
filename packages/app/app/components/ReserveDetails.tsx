d9f6b2440d3ef5579759b1ade66e239609420638
pmsnnogueira <pmsn.mig2002@gmail.com>
2025-06-10 10:51:30 +0100

    style(app): style cards and responsiveness

--- packages/app/app/components/ReserveDetails.tsx
+++ packages/app/app/components/ReserveDetails.tsx
@@ -8,21 +8,21 @@ export function ReserveDetails() {
   if (error) return <div className="text-red-500 font-bold">ERROR: {error.message}</div>
   const toUSD = data ? (value: Value) => data.to(value, 'DJED') : undefined
   return (
-    <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-2 md:p-4 w-full max-w-lg">
+    <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-4 md:p-6 w-full max-w-lg">
       <h2 className="text-xl font-bold mb-6">Reserve Details</h2>
 
       <div className="flex flex-col gap-2">
         <div className="flex flex-row justify-between">
           <p className="font-medium">Min Reserve Ratio</p>
-          <p className="text-lg flex justify-center items-center">{minReserveRatio.toNumber() * 100}%</p>
+          <p className="text-lg flex justify-center items-center text-right">{minReserveRatio.toNumber() * 100}%</p>
         </div>
         <div className="flex flex-row justify-between">
           <p className="font-medium">Max Reserve Ratio</p>
-          <p className="text-lg flex justify-center items-center">{maxReserveRatio.toNumber() * 100}%</p>
+          <p className="text-lg flex justify-center items-center text-right">{maxReserveRatio.toNumber() * 100}%</p>
         </div>
         <div className="flex flex-row justify-between">
           <p className="font-medium">Reserve Ratio</p>
-          <p className="text-lg flex justify-center items-center">
+          <p className="text-lg flex justify-center items-center text-right">
             {isPending ? <LoadingCircle /> : formatNumber(Math.round(data.protocolData.reserve.ratio * 100))}%
           </p>
         </div>
@@ -30,7 +30,7 @@ export function ReserveDetails() {
         <div className="flex flex-row justify-between">
           <p className="font-medium">Reserve Value</p>
           <div className="flex flex-col items-end">
-            <p className="text-lg flex justify-center items-center">
+            <p className="text-lg flex justify-center items-center text-right">
               {isPending ? <LoadingCircle /> : formatValue(data.protocolData.reserve.amount)}
             </p>
             <p className="text-xs text-gray-700 dark:text-gray-400">
--- packages/app/app/components/TokenDetails.tsx
+++ packages/app/app/components/TokenDetails.tsx
@@ -26,7 +26,7 @@ const TokenDetailsRow = ({
     <div className="flex flex-row justify-between">
       <p className="font-medium">{label}</p>
       <div className="flex flex-col items-end">
-        <p className="text-lg">{isPending ? <LoadingCircle /> : formatValue(value ?? {})}</p>
+        <p className="text-lg text-right">{isPending ? <LoadingCircle /> : formatValue(value ?? {})}</p>
         <p className="text-xs text-gray-700 dark:text-gray-400">
           {isPending ? (
             <LoadingCircle />
@@ -46,9 +46,8 @@ export const TokenDetails = ({ token, route }: TokenDetailsProps) => {
   if (error) return <div className="text-red-500 font-bold">ERROR: {error.message}</div>
   const toUSD = data ? (value: Value) => data.to(value, 'DJED') : undefined
   return (
-    <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-4 md:p-6 w-full md:min-w-lg max-w-2xl mx-auto overflow-x-auto">
+    <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-4 md:p-6 w-full mx-auto overflow-x-auto">
       <h2 className="text-2xl font-bold mb-6 ">{token} Token Details</h2>
-
       <div className="flex flex-col gap-6 min-w-fit">
         <div className="grid grid-cols-1 gap-3">
           <TokenDetailsRow
