78f7104e5cb7938b96879de2e64d3fde5ecaa76a
nalmkibb <nunosantos2018@hotmail.com>
2025-06-13 19:26:25 +0100

    style(app): update reserve details

--- packages/app/app/components/ReserveDetails.tsx
+++ packages/app/app/components/ReserveDetails.tsx
@@ -1,43 +1,25 @@
-import { maxReserveRatio, minReserveRatio } from '@reverse-djed/math'
 import { useProtocolData } from '~/hooks/useProtocolData'
 import { formatNumber, formatValue, type Value } from '~/utils'
 import { Skeleton } from './Skeleton'
 import { SkeletonWrapper } from './SkeletonWrapper'
+import { ReserveRatioGraph } from './ReserveRatioGraph'
+import { maxReserveRatio, minReserveRatio } from '@reverse-djed/math'
 
 export function ReserveDetails() {
   const { isPending, error, data } = useProtocolData()
   if (error) return <div className="text-red-500 font-bold">ERROR: {error.message}</div>
   const toUSD = data ? (value: Value) => data.to(value, 'DJED') : undefined
-
+  const currentRatio = data?.protocolData.reserve.ratio ?? 0
   return (
-    <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-4 md:p-6 w-full max-w-lg">
+    <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-2 md:p-4 w-full max-w-lg">
       <h2 className="text-xl font-bold mb-6">Reserve Details</h2>
 
       <div className="flex flex-col gap-2">
-        <div className="flex flex-row justify-between">
-          <p className="font-medium">Min Reserve Ratio</p>
-          <p className="text-lg flex justify-center items-center text-right">
-            {minReserveRatio.toNumber() * 100}%
-          </p>
-        </div>
-
-        <div className="flex flex-row justify-between">
-          <p className="font-medium">Max Reserve Ratio</p>
-          <p className="text-lg flex justify-center items-center text-right">
-            {maxReserveRatio.toNumber() * 100}%
-          </p>
-        </div>
-
-        <div className="flex flex-row justify-between">
-          <p className="font-medium">Reserve Ratio</p>
-          <p className={`text-lg flex ${isPending ? '' : 'items-center justify-center text-right'}`}>
-            {isPending ? (
-              <Skeleton width="w-36" />
-            ) : (
-              `${formatNumber(Math.round(data.protocolData.reserve.ratio * 100))}%`
-            )}
-          </p>
-        </div>
+        <ReserveRatioGraph
+          currentRatio={currentRatio}
+          minRatio={minReserveRatio.toNumber()}
+          maxRatio={maxReserveRatio.toNumber()}
+        />
 
         <div className="flex flex-row justify-between">
           <p className="font-medium">Reserve Value</p>
