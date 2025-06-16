d00569b434d3646adc053613e20f957d07da6cdc
nalmkibb <nunosantos2018@hotmail.com>
2025-06-16 20:13:57 +0100

    fix(app): add skeleton in reserve ratio graph

--- packages/app/app/components/ReserveDetails.tsx
+++ packages/app/app/components/ReserveDetails.tsx
@@ -10,23 +10,20 @@ export function ReserveDetails() {
   if (error) return <div className="text-red-500 font-bold">ERROR: {error.message}</div>
   const toUSD = data ? (value: Value) => data.to(value, 'DJED') : undefined
   const currentRatio = data?.protocolData.reserve.ratio ?? 0
-  const currentRatio = data?.protocolData.reserve.ratio ?? 0
   return (
-    <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-2 md:p-4 w-full max-w-lg">
     <div className="bg-light-foreground dark:bg-dark-foreground shadow-md rounded-xl p-2 md:p-4 w-full max-w-lg">
       <h2 className="text-xl font-bold mb-6">Reserve Details</h2>
 
       <div className="flex flex-col gap-2">
-        <ReserveRatioGraph
-          currentRatio={currentRatio}
-          minRatio={minReserveRatio.toNumber()}
-          maxRatio={maxReserveRatio.toNumber()}
-        />
-        <ReserveRatioGraph
-          currentRatio={currentRatio}
-          minRatio={minReserveRatio.toNumber()}
-          maxRatio={maxReserveRatio.toNumber()}
-        />
+        {isPending ? (
+          <Skeleton width="w-36" />
+        ) : (
+          <ReserveRatioGraph
+            currentRatio={currentRatio}
+            minRatio={minReserveRatio.toNumber()}
+            maxRatio={maxReserveRatio.toNumber()}
+          />
+        )}
 
         <div className="flex flex-row justify-between">
           <p className="font-medium">Reserve Value</p>
