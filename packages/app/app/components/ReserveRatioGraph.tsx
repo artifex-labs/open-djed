1502e3537e8cf0635c568c25cd37e8c1367a4c39
pmsnnogueira <pmsn.mig2002@gmail.com>
2025-06-17 20:26:58 +0100

    style(app): reserveRatio tooltip

--- packages/app/app/components/ReserveRatioGraph.tsx
+++ packages/app/app/components/ReserveRatioGraph.tsx
@@ -12,29 +12,27 @@ export function ReserveRatioGraph({ currentRatio, minRatio, maxRatio }: ReserveR
     {
       label: 'Min',
       value: minRatio,
-      position: 'top-full mt-10',
+      position: 'top-full mt-1',
       style: 'w-1 h-5 bg-black dark:bg-white',
     },
     {
       label: 'Max',
       value: maxRatio,
-      position: 'top-full mt-10',
+      position: 'top-full mt-1',
       style: 'w-1 h-5 bg-black dark:bg-white',
     },
     {
       label: 'Current',
       value: currentRatio,
-      position: 'bottom-full mb-12',
-      style:
-        'w-5 h-5 rounded-full border-2 border-black bg-white dark:bg-black dark:border-white hover:shadow-lg transition-shadow',
+      position: 'bottom-full mb-1',
+      style: 'w-5 h-5 rounded-full border-2 border-black bg-white dark:bg-black dark:border-white',
+      tooltipModalClass: 'py-4',
     },
   ]
 
   return (
     <div className="flex flex-col gap-2 mb-4">
       <div className="relative w-full h-6 rounded-lg overflow-visible">
-        <div className="absolute top-1/2 left-0 w-full h-3 bg-white dark:bg-gray-900 rounded-lg transform -translate-y-1/2 z-0 shadow-inner" />
-
         <div className="absolute top-1/2 left-0 h-3 w-full flex overflow-hidden z-10 -translate-y-1/2 rounded-lg">
           <div
             className="bg-amber-500 rounded-l-lg transition-all duration-300 ease-in-out"
@@ -49,27 +47,26 @@ export function ReserveRatioGraph({ currentRatio, minRatio, maxRatio }: ReserveR
             style={{ width: `${100 - (maxRatio / 10) * 100}%` }}
           />
         </div>
-
-        {reserves.map(({ label, value, position, style }, index) => (
-          <div key={index}>
-            <div
-              className="absolute z-20 top-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ease-in-out"
-              style={{ left: `${(value / 10) * 100}%` }}
+        {reserves.map(({ label, value, position, style, tooltipModalClass }, index) => (
+          <div
+            key={index}
+            className="absolute top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 group"
+            style={{ left: `${(value / 10) * 100}%` }}
+          >
+            <Tooltip
+              text={`${formatNumber(Math.round(value * 100), { minimumFractionDigits: 0 })}%`}
+              style={{ display: 'contents' }}
+              tootlipModalClass={tooltipModalClass || ''}
             >
-              <div className={style} />
-            </div>
-            <div
-              className="absolute z-20 flex flex-col items-center transform -translate-x-1/2 -translate-y-1/2 top-1/2 transition-all duration-300 ease-in-out"
-              style={{ left: `${(value / 10) * 100}%` }}
-            >
-              <div
-                className={`text-xs font-semibold text-black dark:text-white ${position} hover:scale-110 transition-transform`}
-              >
-                <Tooltip text={`${formatNumber(Math.round(value * 100), { minimumFractionDigits: 0 })}%`}>
+              <div className="relative flex flex-col items-center justify-center">
+                <div className={style} />
+                <div
+                  className={`absolute text-xs font-semibold text-black dark:text-white ${position} group-hover:scale-110 transition-transform`}
+                >
                   {label}
-                </Tooltip>
+                </div>
               </div>
-            </div>
+            </Tooltip>
           </div>
         ))}
       </div>
--- packages/app/app/components/Tooltip.tsx
+++ packages/app/app/components/Tooltip.tsx
@@ -1,19 +1,23 @@
 interface TooltipProps {
   text: string
   tooltipDirection?: 'top' | 'bottom' | 'left' | 'right'
+  style?: React.CSSProperties
+  tootlipModalClass?: string
   children?: React.ReactNode
 }
 
-const Tooltip = ({ text, tooltipDirection = 'top', children }: TooltipProps) => {
-  const tooltipClass = `tooltip tooltip-${tooltipDirection}`
+const Tooltip = ({ text, tooltipDirection = 'top', style, tootlipModalClass, children }: TooltipProps) => {
+  const tooltipClass = `tooltip tooltip-${tooltipDirection}`.trim()
+  const tootlipModalStyle = `tooltip-content ${tootlipModalClass}`.trim()
 
   return (
-    <div className={tooltipClass}>
-      <div className="tooltip-content">
+    <div className={tooltipClass} style={style}>
+      <div className={tootlipModalStyle}>
         <div className="bg-white dark:bg-black rounded-lg p-2 opacity-95">{text}</div>
       </div>
-      {children || <i className="fa-solid fa-circle-info"></i>}
+      {children ?? <i className="fa-solid fa-circle-info" />}
     </div>
   )
 }
+
 export default Tooltip
